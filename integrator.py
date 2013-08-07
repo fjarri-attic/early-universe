53e69aab5343a104a2e4f702dbf8ac3bd361832a
Bogdan Opanchuk <bogdan@opanchuk.net>
2013-08-07 15:47:33 +1000

    integrator: meshgrid does not want to create a grid out of only one array

--- integrator.py
+++ integrator.py
@@ -15,7 +15,12 @@ def get_ksquared(shape, box):
     ks = [
         2 * numpy.pi * numpy.fft.fftfreq(size, length / size)
         for size, length in zip(shape, box)]
-    full_ks = numpy.meshgrid(*ks, indexing='ij')
+
+    if len(shape) > 1:
+        full_ks = numpy.meshgrid(*ks, indexing='ij')
+    else:
+        full_ks = [ks]
+
     return sum([full_k ** 2 for full_k in full_ks])
 
 
